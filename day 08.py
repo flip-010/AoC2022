64f5efa4c5f6c90cabd589fb3588dadfebe4f314
flip-010 <98099049+flip-010@users.noreply.github.com>
2022-12-14 13:32:48 +0100

    Add files via upload

--- day 08.py
+++ day 08.py
@@ -1,5 +1,4 @@
 import time
-import re
 
 start_time = time.time()
 
@@ -7,22 +6,13 @@ with open('C:/Users/fbukman/OneDrive - Capgemini/Desktop/pgm/VS/AoC2021/2022/day
     lines = fp.readlines()
     lines=[line.rstrip() for line in lines]
 
-veld = ['' for i in range(99)]
-
-
-
-
-print('deel 1:', 0)
-
+print('deel 1:', 'in excel')
 
 som = 0
 for i in range(1,98):
     for j in range(1,98):
         w = int(lines[i][j])
-        t1 = 1
-        t2 = 1
-        t3 = 1
-        t4 = 1
+        t1, t2, t3, t4 = 1,1,1,1
         a = i - 1
         while a > 0 and w > int(lines[a][j]):
             a -= 1
@@ -39,10 +29,7 @@ for i in range(1,98):
         while a < 98 and w > int(lines[i][a]):
             a += 1
             t4 += 1
-        fact = t1*t2*t3*t4
-        if fact > som: som = fact
+        som = max(som, t1*t2*t3*t4)
 print('deel 2:', som)
-
-
 
 print("--- %s seconds ---" % (time.time() - start_time))
